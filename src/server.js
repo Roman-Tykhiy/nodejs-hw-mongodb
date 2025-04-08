11535dc6fb484080a17717bdfe8a582ceebaaa5f
Roman_Tykhiy <2rstykhiy@gmail.com>
2025-04-08 09:47:15 +0300

    HW2

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ export const setupServer = () => {
             target: "pino-pretty"
         }
     }));
-    app.get("/api/contacts", async (req, res) => {
+    app.get("/contacts", async (req, res) => {
         const data = await getContacts();     
         res.json({
             status: 200,
@@ -22,7 +22,7 @@ export const setupServer = () => {
              
         })
     });
-    app.get("/api/contacts/:contactId", async (req, res) => {
+    app.get("/contacts/:contactId", async (req, res) => {
         const { contactId } = req.params;
         
         
